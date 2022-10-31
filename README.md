ef5a940d165ac898c9a2111daea1a3f6e849145c
Sotatek-KhanhHo <57700128+khanhhq173191@users.noreply.github.com>
2022-10-31 23:30:36 +0700

    first

---
+++ README.md
@@ -0,0 +1,23 @@
+# Tặng Crush
+## _Một điều nho nhỏ tỏ tình với crush_
+
+Mở file config.js sửa nội dung theo mẫu
+```
+const CONFIG = {
+    introTitle: '............. à!',
+    introDesc: `.............`,
+    btnIntro: '.............',
+    title: '.............',
+    desc: '.............',
+    btnYes: '.............',
+    btnNo: '.............',
+    question:'.............',
+    btnReply: '.............',
+    reply: '.............',
+    mess: '.............',
+    messDesc: '.............',
+    btnAccept: '.............',
+    messLink: 'http://fb.com' //link mess của các bạn
+}
+```
+
---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  introTitle: "Xin Chào!!!!",
+  introDesc: `Chào mừng bạn đến với thế giới của Khánh trưởng thành (Quên đi Khánh trẻ trâu đi nhé)`,
+  btnIntro: "Oke",
+  title: "Thấy Khánh giờ khác bọt không",
+  desc: "Không trả lời hoặc thoát ra, tức là cậu đồng ý đó :> ",
+  btnYes: "Chuẩn luôn giờ Khánh ngầu hơn nhiều rồi",
+  btnNo: "Vẫn còn trẻ trâu lắm =))",
+  question: "Bạn có muốn biết thêm về mình khum",
+  btnReply: "Gửi cho cho Khánh của hiện tại",
+  reply: "Khánh giờ trưởng thành và ngầu hơn trước nhìu lắm :)))))))",
+  mess: "Tớ biết mà 🥰. Cảm ơn cậu rất nhìu 😘😘",
+  messDesc: "còn giờ thì chờ gì nữa mà không inbox cho Khánh để nhận quà nào.",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://www.facebook.com/profile.php?id=100019094589612", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
